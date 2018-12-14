5435a4896b2feafae9731ca2a09bf15798e5947a
junz <junz@alleninstitute.org>
2018-12-13 17:06:26 -0800

    change to pyqt5 style

--- eyetracker/eyetrackerGUI.py
+++ eyetracker/eyetrackerGUI.py
@@ -1,17 +1,17 @@
 import sys
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 from mainwindow_layout import Ui_MainWindow
 
-class EyetrackerGui(QtGui.QMainWindow):
+class EyetrackerGui(QtWidgets.QMainWindow):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     myapp = EyetrackerGui()
     myapp.show()
     sys.exit(app.exec_())
