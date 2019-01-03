c0b998ac763bead869b3861604571489221685e8
junz <junz@alleninstitute.org>
2019-01-03 13:50:32 -0800

    add .avi to .gitignore

--- eyetracker/tests/test_files/get_movie_chunk.py
+++ eyetracker/tests/test_files/get_movie_chunk.py
@@ -3,9 +3,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-fn = r"F:\data2\rabies_tracing_project\M360495\2018-03-23-deepscope-reanalysis" \
-     r"\video_mon\180323-M360495-CombinedStimuli-100-0.avi"
-frame_range = [5800, 5900]
+fn = r"F:\data2\rabies_tracing_project\M417949\2018-12-14-deepscope\videomon\181214-M417949-LSNDGC-110--1.avi"
+frame_range = [117220, 117380]
 
 curr_folder = os.path.dirname(os.path.realpath(__file__))
 os.chdir(curr_folder)
@@ -15,7 +14,7 @@ print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 frame_shape = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-vw = cv2.VideoWriter(filename=os.path.join(curr_folder, 'test2.avi'),
+vw = cv2.VideoWriter(filename=os.path.join(curr_folder, 'test3.avi'),
                      fourcc=cv2.VideoWriter_fourcc(*'XVID'),
                      fps=30.,
                      frameSize=frame_shape)
